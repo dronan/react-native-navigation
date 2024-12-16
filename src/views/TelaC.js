10eabe3dc98fa948bcde8bd97641b24accc7e7a1
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 12:32:41 +0100

    garantindo o recebimento de um valor nao nulo

--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -2,9 +2,10 @@ import React from 'react';
 import TextoCentral from '../components/TextoCentral';
 
 export default props => {
+  const route = props.route || {params: {numero: 0}};
   return (
     <TextoCentral corFundo="#C93" style={{flex: 1}}>
-      Tela C - {props.route.params.numero}
+      Tela C {route.params.numero}
     </TextoCentral>
   );
 };
