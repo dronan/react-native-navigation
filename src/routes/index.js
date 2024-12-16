c36d3892619ee12ca253909b3ae9a4ae52081212
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 13:01:01 +0100

    usando o drawer e instalando os pacotes e referencias para funcionamento correto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import Stack from './Stack';
-import Tab from './Tab';
+import Drawer from './Drawer';
 
 export default () => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
-      <Tab />
+      <Drawer />
     </NavigationContainer>
   </SafeAreaView>
 );
