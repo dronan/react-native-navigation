d37244e5fd13c03122dfd3574e0051bd5b1e71ad
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 12:44:59 +0100

    exemplo de navegacao por tabs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Stack from './Stack';
+import Tab from './Tab';
 
 export default () => (
   <SafeAreaView style={{flex: 1}}>
     <NavigationContainer>
-      {/* Stack Navigator */}
-      <Stack />
+      <Tab />
     </NavigationContainer>
   </SafeAreaView>
 );
