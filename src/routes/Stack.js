1f6d29da5269366aa058142f3919b8b102e7ca90
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-16 12:19:39 +0100

    outra forma de implementar o avançar

--- src/routes/Stack.js
+++ src/routes/Stack.js
@@ -3,17 +3,26 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
 import TelaC from '../views/TelaC';
+import PassoStack from '../components/PassoStack';
 
 const Stack = createNativeStackNavigator();
 
 export default () => (
   <Stack.Navigator initialRouteName="TelaA" options={{headerShown: true}}>
-    <Stack.Screen
-      name="TelaA"
-      options={{title: 'Informações Iniciais'}}
-      component={TelaA}
-    />
-    <Stack.Screen name="TelaB" component={TelaB} />
+    <Stack.Screen name="TelaA" options={{title: 'Informações Iniciais'}}>
+      {props => (
+        <PassoStack {...props} avancar="TelaB">
+          <TelaA />
+        </PassoStack>
+      )}
+    </Stack.Screen>
+    <Stack.Screen name="TelaB">
+      {props => (
+        <PassoStack {...props} avancar="TelaC">
+          <TelaB />
+        </PassoStack>
+      )}
+    </Stack.Screen>
     <Stack.Screen name="TelaC" component={TelaC} />
   </Stack.Navigator>
 );
--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import TextoCentral from '../components/TextoCentral';
-import PassoStack from '../components/PassoStack';
 
 export default props => {
   return (
-    <PassoStack {...props} avancar="TelaB">
-      <TextoCentral corFundo="#E53935" style={{flex: 1}}>
-        Tela A
-      </TextoCentral>
-    </PassoStack>
+    <TextoCentral corFundo="#E53935" style={{flex: 1}}>
+      Tela A
+    </TextoCentral>
   );
 };
